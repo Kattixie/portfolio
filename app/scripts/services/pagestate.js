04ec647f6bffa43c71def4bcc92ce6f2e21777aa
Jessica Ann <kattixie@rukes.net>
2016-02-02 22:20:55 -0800

    Tightened amount of header space in small device styles. Added comment about using ng-bind for title.

--- app/scripts/services/pagestate.js
+++ app/scripts/services/pagestate.js
@@ -38,7 +38,9 @@ angular
             // This is currently set up so that we don't have to create a
             // controller for the page title. However, if we want to avoid
             // using rootScope, we could probably do this. We could also add a
-            // directive to the title.
+            // directive to the title. Although, it sounds like there are some
+            // problems with the page title template without the use of
+            // ng-bind: http://stackoverflow.com/a/22467512
             $rootScope.title = newTitle;
         };
 
